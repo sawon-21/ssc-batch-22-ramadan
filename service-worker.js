7016b549524aed7e4dff03006342c709926c755d
sawon-21 <businessmansawon@gmail.com>
2025-04-08 21:20:50 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,65 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-});
+// Cache name
+const CACHE_NAME = 'my-cache-v-0001';
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activated.');
-});
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/Quran.jpg',
+  '/poster.jpg',
+  'https://quran.com/bn',
+  'https://fonts.googleapis.com/css?family=Noto+Sans+Bengali:300,400,700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://fonts.maateen.me/apona-lohit/font.css'
+];
 
-self.addEventListener('push', function(event) {
-  const options = {
-    body: 'This is your scheduled Ramadan reminder!',
-    icon: 'icon.png',
-    tag: 'ramadan-reminder'
-  };
+// Install event
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    self.registration.showNotification('Ramadan Reminder', options)
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Push Notification Handler
+self.addEventListener('push', function (event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || "Ramadan Reminder";
+  const options = {
+    body: data.body || "Time for a short dua and reflection!",
+    icon: 'logo.png',
+    badge: 'logo.png',
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
   );
 });
